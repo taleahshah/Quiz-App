9d6f70bd9fef7df0f221b4f48e94aa932988219a
Taleah Shah <51045892+taleahshah@users.noreply.github.com>
2021-01-22 09:41:45 +0500

    Add files via upload

---
+++ src/Quiz.js
@@ -0,0 +1,140 @@
+import React, { Component } from 'react'
+import { QuizData } from './QuizData';
+import './styles.css'
+
+class Quiz extends Component {
+
+
+
+    state = {
+        userAnswer: null,    //current users answer
+        currentIndex: 0,  //current questions index
+        options: [],       //the four options
+        quizEnd: false,
+        score: 0,
+        disabled: true
+    }
+
+
+
+
+    loadQuiz = () => {
+        const { currentIndex } = this.state
+        this.setState(() => {
+            return {
+                question: QuizData[currentIndex].question,
+                options: QuizData[currentIndex].options,
+                answer: QuizData[currentIndex].answer
+            }
+        })
+    }
+
+
+    nextQuestionHander = () => {
+        const { userAnswer, answer, score } = this.state
+
+        if (userAnswer === answer) {
+            this.setState({
+                score: score + 1
+            })
+        }
+        this.setState({
+            currentIndex: this.state.currentIndex + 1,
+            userAnswer: null
+        })
+    }
+
+
+
+
+    componentDidMount() {
+        this.loadQuiz();
+    }
+
+
+    checkAnswer = answer => {
+        this.setState({
+            userAnswer: answer,
+            disabled: false
+        })
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+        const { currentIndex } = this.state;
+        if (this.state.currentIndex !== prevState.currentIndex) {
+            this.setState(() => {
+                return {
+                    disabled: true,
+                    question: QuizData[currentIndex].question,
+                    options: QuizData[currentIndex].options,
+                    answer: QuizData[currentIndex].answer
+                }
+            });
+
+        }
+    }
+
+
+
+
+    finishHandler = () => {
+        if (this.state.currentIndex === QuizData.length - 1) {
+            this.setState({
+                quizEnd: true
+            })
+        }
+
+    }
+
+    render() {
+        const { question, options, currentIndex, userAnswer, quizEnd } = this.state
+        if (quizEnd) {
+            return (
+                <div>
+                    <h1>Game Over. Final score is {this.state.score} points</h1>
+                    <p>The correct Answers for the quiz are</p>
+                    <ul>
+                        {QuizData.map((item, index) => (
+                            <li className='options'
+                                key={index}>
+                                {item.answer}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
+
+        return (
+            <div>
+                <h2>{question}</h2>
+                <span>{`Question ${currentIndex + 1} of ${QuizData.length}`}</span>
+                {options.map(option => (
+                    <p key={option.id}
+                        className={`options ${userAnswer === option ? "selected" : null}`}
+                        onClick={() => this.checkAnswer(option)}>
+                        {option}
+                    </p>
+                ))}
+                {currentIndex < QuizData.length - 1 &&
+
+                    <button
+                        className="ui inverted button"
+                        disabled={this.state.disabled}
+                        onClick={this.nextQuestionHander}
+                    >Next Question</button>
+                }
+                {currentIndex === QuizData.length - 1 &&
+                    <button
+                        className="ui inverted button"
+                        disabled={this.state.disabled}
+                        onClick={this.finishHandler}
+                    >Finish</button>
+                }
+            </div>
+        )
+    }
+}
+
+export default Quiz
